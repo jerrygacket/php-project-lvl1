8d76b56ebcbc44f5f4d77b48f61d7d527feaf73f
Крючков Артём Сергеевич <jerrygacket@gmail.com>
2021-10-29 21:52:24 +0300

    step 5 brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,25 @@ use function cli\prompt;
 
 function hello()
 {
-    line('Welcome to the Brain Game!');
+    line('');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    return $name;
+}
+
+function startGame()
+{
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+function getNumber()
+{
+    return rand(1, 100);
+}
+
+function checkNumber($num)
+{
+    return ($num % 2) === 0 ? 'yes' : 'no';
 }
